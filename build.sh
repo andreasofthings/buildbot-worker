268187cef1d5c6d13de5b6c42f5c834d76148a91
Andreas Neumeier <andreas@neumeier.org>
2018-02-26 23:04:55 +0100

    last push

--- build.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login --region eu-central-1
-docker build -t pramari-buildbot-worker .
-docker tag pramari-buildbot-worker:latest 511053357167.dkr.ecr.eu-central-1.amazonaws.com/pramari-buildbot-worker:latest
-docker push 511053357167.dkr.ecr.eu-central-1.amazonaws.com/pramari-buildbot-worker:latest
